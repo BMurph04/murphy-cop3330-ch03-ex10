8078dcc86ddcf48a93c02336ebd8d0d596cd72cd
Brendon <murph4012@gmail.com>
2021-11-26 02:34:31 -0500

    Initial commit

---
+++ calculator.h
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Brendon Murphy
+ */
+
+#include <iostream>
+#include <string>
+int calculator(int num1, int num2, std::string op);
+
+
+
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "calculator.h"
+
+using namespace std;
+
+int main(void){
+    int num1, num2;
+    string op;
+    
+    cout << "Please enter the operation you wish to use, followed by the first and second operands." << endl;
+    cin >> op >> num1 >> num2;
+    
+    int result = calculator(num1, num2, op);
+    cout << "The result is " << result << "." << endl;
+
+    return 0;
+}
+
+int calculator(int num1,int num2, string op){
+    if(op == "+"){
+        return num1+num2;
+    }
+    else if(op == "-"){
+        return num1-num2;
+    }
+    else if(op == "*"){
+        return num1*num2;
+    }
+    else if(op == "/"){
+        return num1/num2;
+    }
+    return 0;
+}
