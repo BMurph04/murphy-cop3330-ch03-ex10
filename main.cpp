059360bc247f19f68dcf681eac1b7e4a02aff593
Brendon <murph4012@gmail.com>
2021-11-26 02:55:05 -0500

    2nd commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main(void){
     
     cout << "Please enter the operation you wish to use, followed by the first and second operands." << endl;
     cin >> op >> num1 >> num2;
+    if(!(op == "+" || op == "-" || op == "*" || op == "/")){
+        cout << "Invalid operation." << endl;
+        return 0;
+    }
     
     int result = calculator(num1, num2, op);
     cout << "The result is " << result << "." << endl;
